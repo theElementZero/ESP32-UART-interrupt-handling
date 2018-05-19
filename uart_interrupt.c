58768256376c5290e041d277600b13c097383593
Sushant <sushantnaik21@gmail.com>
2018-05-19 15:47:41 +0530

    Update uart_interrupt.c

--- uart_interrupt.c
+++ uart_interrupt.c
@@ -1,6 +1,5 @@
-/* UART Interrupt Example
-
-
+/* 
+UART Interrupt Example
 */
 #include <stdio.h>
 #include <string.h>
@@ -69,23 +68,23 @@ void blink_task(void *pvParameter)
  */
 static void IRAM_ATTR uart_intr_handle(void *arg)
 {
-	uint16_t rx_fifo_len, status;
+  uint16_t rx_fifo_len, status;
   uint16_t i;
   
-	status = UART0.int_st.val; // read UART interrupt Status
-	rx_fifo_len = UART0.status.rxfifo_cnt; // read number of bytes in UART buffer
+  status = UART0.int_st.val; // read UART interrupt Status
+  rx_fifo_len = UART0.status.rxfifo_cnt; // read number of bytes in UART buffer
   
-	while(rx_fifo_len){
-	  rxbuf[i++] = UART0.fifo.rw_byte; // read all bytes
-	  rx_fifo_len--;
-	}
+  while(rx_fifo_len){
+   rxbuf[i++] = UART0.fifo.rw_byte; // read all bytes
+   rx_fifo_len--;
+ }
   
-	// after reading bytes from buffer clear UART interrupt status
-	uart_clear_intr_status(EX_UART_NUM, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
+ // after reading bytes from buffer clear UART interrupt status
+ uart_clear_intr_status(EX_UART_NUM, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
 
-  // a test code or debug code to indicate UART receives successfully,
-  // you can redirect received byte as echo also
-	uart_write_bytes(EX_UART_NUM, (const char*) "RX Done", 7);
+// a test code or debug code to indicate UART receives successfully,
+// you can redirect received byte as echo also
+ uart_write_bytes(EX_UART_NUM, (const char*) "RX Done", 7);
 
 }
 /*
@@ -93,43 +92,44 @@ static void IRAM_ATTR uart_intr_handle(void *arg)
  */
 void app_main()
 {
-   int ret;
-    esp_log_level_set(TAG, ESP_LOG_INFO);
+	int ret;
+	esp_log_level_set(TAG, ESP_LOG_INFO);
 
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    
-    ESP_ERROR_CHECK(uart_param_config(EX_UART_NUM, &uart_config));
+	/* Configure parameters of an UART driver,
+	* communication pins and install the driver */
+	uart_config_t uart_config = {
+		.baud_rate = 115200,
+		.data_bits = UART_DATA_8_BITS,
+		.parity = UART_PARITY_DISABLE,
+		.stop_bits = UART_STOP_BITS_1,
+		.flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+	};
 
-    //Set UART log level
-    esp_log_level_set(TAG, ESP_LOG_INFO);
-    
-    //Set UART pins (using UART0 default pins ie no changes.)
-    ESP_ERROR_CHECK(uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    
-    //Install UART driver, and get the queue.
-    ESP_ERROR_CHECK(uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, 0, 0, NULL, 0));
+	ESP_ERROR_CHECK(uart_param_config(EX_UART_NUM, &uart_config));
 
-    // release the pre registered UART handler/subroutine
-    ESP_ERROR_CHECK(uart_isr_free(EX_UART_NUM));
-   
-    // register new UART subroutine
-    ESP_ERROR_CHECK(uart_isr_register(EX_UART_NUM,uart_intr_handle, NULL, ESP_INTR_FLAG_IRAM, &handle_console));
+	//Set UART log level
+	esp_log_level_set(TAG, ESP_LOG_INFO);
 
-    ESP_ERROR_CHECK(uart_enable_rx_intr(EX_UART_NUM));
-#if (NOTASK == 1)
-    while(1)
-    {
-    	vTaskDelay(1000);
-    }
-#else
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-#endif
+	//Set UART pins (using UART0 default pins ie no changes.)
+	ESP_ERROR_CHECK(uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+	//Install UART driver, and get the queue.
+	ESP_ERROR_CHECK(uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, 0, 0, NULL, 0));
+
+	// release the pre registered UART handler/subroutine
+	ESP_ERROR_CHECK(uart_isr_free(EX_UART_NUM));
+
+	// register new UART subroutine
+	ESP_ERROR_CHECK(uart_isr_register(EX_UART_NUM,uart_intr_handle, NULL, ESP_INTR_FLAG_IRAM, &handle_console));
+
+	// enable RX interrupt
+	ESP_ERROR_CHECK(uart_enable_rx_intr(EX_UART_NUM));
+	#if (NOTASK == 1)
+	while(1)
+	{
+		vTaskDelay(1000);
+	}
+	#else
+		xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+	#endif
 }
